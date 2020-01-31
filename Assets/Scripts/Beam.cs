decdc35bd821576dc08ba3a85b04d5078509174f
Pastew <kpastew@gmail.com>
2020-01-31 20:27:44 +0100

    init

---
+++ Assets/Scripts/Beam.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Beam : MonoBehaviour
+{
+    private float _delayLeft = 0;
+
+    [SerializeField] private float _delay = 1.5f;
+    [SerializeField] private float _duration = 0.3f;
+    [SerializeField] private float _moveX = 2f;
+    [SerializeField] private AnimationCurve _moveAnimCurve;
+
+    void Update()
+    {
+        _delayLeft -= Time.deltaTime;
+        
+        if (_delayLeft <= 0)
+        {
+            Move();
+            _delayLeft = _delay;
+        }
+    }
+    private void Move()
+    {
+        transform.DOMoveX(transform.position.x - _moveX, _duration).SetEase(_moveAnimCurve);
+    }
+}
