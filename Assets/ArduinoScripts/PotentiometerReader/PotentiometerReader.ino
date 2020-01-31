4b85c7af1e0772f0e2b41b37fc00797c4d7c3448
Pastew <kpastew@gmail.com>
2020-02-01 00:40:25 +0100

    Arduino connection works!

---
+++ Assets/ArduinoScripts/PotentiometerReader/PotentiometerReader.ino
@@ -0,0 +1,18 @@
+int potPin = 2;
+int potVal = 0;
+
+void setup() {
+  Serial.begin(250000); // You can choose any baudrate, just need to also change it in Unity.
+  //while (!Serial); // wait for Leonardo enumeration, others continue immediately
+}
+
+void loop() {
+  potVal = analogRead(potPin);
+  potVal = map(potVal, 0, 1023, 0, 100);
+  sendData(potVal);
+  delay(5); // Choose your delay having in mind your ReadTimeout in Unity3D
+}
+
+void sendData(int data) {
+  Serial.println(data); // need a end-line because wrmlh.csharp use readLine method to receive data
+}
---
+++ Assets/Scripts/PotValueText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotValueText : MonoBehaviour
+{
+    private Text _text;
+
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        _text.text = Potentiometer.Value.ToString();
+    }
+}
