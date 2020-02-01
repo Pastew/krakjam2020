b82e6e91ea7b4fc5174f267e795868053584b60c
Pastew <kpastew@gmail.com>
2020-02-02 00:34:30 +0100

    Refactor, cleanup

--- Assets/ArduinoScripts/PotentiometerReader/PotentiometerReader.ino
+++ Assets/ArduinoScripts/PotentiometerReader/PotentiometerReader.ino
@@ -8,6 +8,7 @@ void setup() {
 
 void loop() {
   potVal = analogRead(potPin);
+  potVal = 1023 - potVal;
   potVal = map(potVal, 0, 1023, 0, 1000);
   sendData(potVal);
   delay(5); // Choose your delay having in mind your ReadTimeout in Unity3D
