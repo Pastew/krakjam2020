cf3c2af490b7150c5444c4af950782b3fdb38520
Pastew <kpastew@gmail.com>
2020-02-01 16:28:09 +0100

    added multiplier to screw

--- Assets/Scripts/Screw.cs
+++ Assets/Scripts/Screw.cs
@@ -4,25 +4,31 @@ using Random = System.Random;
 public class Screw : MonoBehaviour
 {
     [SerializeField] private ScrewCanvas _screwCanvas;
-    
-    public int Level { get; set; }
-    private bool active = false;
 
+    private bool active = false;
+    
+    private float _level;
+    public int Level => (int) _level;
+
+    [SerializeField] private float _multiplier = 0.5f;
     private int _previousPotValue;
 
     private void OnEnable()
     {
         _previousPotValue = 100;
-        Level = new Random().Next(0, 80);
+        _level = new Random().Next(0, 80);
     }
 
     void Update()
     {
+        if(!active)
+            return;
+
         int diff = Potentiometer.Value - _previousPotValue;
         _previousPotValue = Potentiometer.Value;
         
         if (diff > 0)
-            Level += diff;
+            _level += diff * _multiplier;
     }
 
     public void Activate()
