e2bbc3b43079f42ad662ebbd3c1fee993afde990
Pastew <kpastew@gmail.com>
2020-02-02 13:49:56 +0100

    ADded no arduino support. FINISHgit add .git add .git add .!

---
+++ Assets/Scripts/ArduinoStub.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ArduinoStub : MonoBehaviour
+{
+    private Potentiometer _pot;
+    private Slider _slider;
+
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+        
+        _pot = FindObjectOfType<Potentiometer>();
+        if(!_pot.noArduino)
+            Destroy(gameObject);
+    }
+    
+    void Update()
+    {
+        Potentiometer.Value = (int) _slider.value;
+    }
+}
